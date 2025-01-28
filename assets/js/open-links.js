1dbfbcf81e839d02abb3e7ac6a02df738470debf
OpusOpenFinance <open.finance@opus-software.com.br>
2025-01-28 19:40:49 -0300

    Criação de dir "/assets/js"

--- assets/js/open-links.js
+++ assets/js/open-links.js
@@ -1,7 +1,7 @@
 (function () {
     document.addEventListener("DOMContentLoaded", function () {
     // Observa mudanças no DOM, para lidar com carregamento dinâmico.
-        console.log("Script isolado carregado em /docs/assets!");
+        console.log("Script isolado carregado em /assets!");
         const links = document.querySelectorAll("a[href^='http']"); // Captura http e https.
         links.forEach(function (link) {
             if (!link.href.includes(window.location.hostname)) {    // Verifica se link é externo.
